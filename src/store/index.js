238bb56052b26d41b456acc233f161d77a59cc99
Kridsada Wannasing <kridsadawannasing1996@gmaill.com>
2020-09-10 17:50:08 +0700

    teacher services and vuex store

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,12 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import * as teacher from "../store/modules/teacher";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
+  modules: { teacher },
   state: {},
   mutations: {},
   actions: {},
-  modules: {}
 });
