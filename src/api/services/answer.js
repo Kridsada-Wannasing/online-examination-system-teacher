42560e1360cdd8c6f5023e2fd56e1503e94c0301
Kridsada Wannasing <kridsadawannasing1996@gmaill.com>
2020-09-20 23:53:56 +0700

    add answer vuex store

---
+++ src/api/services/answer.js
@@ -0,0 +1,18 @@
+import { apiClient } from "../axios";
+
+const endpoint = "answer";
+
+export default {
+  createAnswer(answer) {
+    return apiClient.post(`/${endpoint}/login`, answer);
+  },
+  getAllAnswers(questionId) {
+    return apiClient.get(`/${endpoint}/${questionId}`);
+  },
+  updateAnswer(answer) {
+    return apiClient.patch(`/${endpoint}/`, answer);
+  },
+  deleteAnswer(answerId) {
+    return apiClient.delete(`${endpoint}/`, answerId);
+  },
+};
